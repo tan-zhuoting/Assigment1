12937c51b443603a8c3cfc36734420feb48dba4e
Zhuoting Tan <zhuoting@ucla.edu>
2020-04-27 01:17:24 -0700

    Add template provided, add more member functions for each class

--- cards.h
+++ cards.h
@@ -1,36 +1,105 @@
-#pragma once
-class Player
-{
+/* *************************************
+   Zhuoting Tan, April 26, 2020
+
+   Interface of a simple Card class
+   Used template by Professor Salazar
+   ************************************* */
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#ifndef CARDS_H
+#define CARDS_H
+
+using namespace std;
+
+enum suit_t { OROS, COPAS, ESPADAS, BASTOS };
+
+/*
+The values for this type start at 0 and increase by one
+afterwards until they get to SIETE.
+The rank reported by the function Card::get_rank() below is
+the value listed here plus one.
+E.g:
+The rank of AS is reported as    static_cast<int>(AS) + 1   = 0 + 1 =  1
+The rank of SOTA is reported as  static_cast<int>(SOTA) + 1 = 9 + 1 = 10
+*/
+enum rank_t { AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA = 9, CABALLO = 10, REY = 11 };
+
+class Card {
 public:
-	Player();
-	~Player();
+    // Constructor assigns random rank & suit to card.
+    Card();
+
+    // Accessors 
+    string get_spanish_suit() const;
+    string get_spanish_rank() const;
+
+    /*
+       These are the only functions you'll need to code
+       for this class. See the implementations of the two
+   functions above to get an idea of how to proceed.
+    */
+    string get_english_suit() const;
+    string get_english_rank() const;
+
+    // Converts card rank to number.
+    // The possible returns are: 1, 2, 3, 4, 5, 6, 7, 10, 11 and 12
+    int get_rank() const;
+
+    // Compare rank of two cards. E.g: Eight<Jack is true.
+    // Assume Ace is always 1. 
+    // Useful if you want to sort the cards.
+    bool operator < (Card card2) const;
+
+    //return the value of the card
+    double get_val() const;
+private:
+    suit_t suit;
+    rank_t rank;
+};
+
+class Hand {
+public:
+    // A vector of Cards
+    Hand();
+
+    void print();
+    void draw();
+    bool operator>(Hand& d);
+    void empty(); //Empty cards in hand to start the next game
+    
 
 private:
-
+    vector<Card> cards;
+    double total;
+    // You decide what fields you'll need...
 };
 
 
-class Hand
-{
+class Player {
 public:
-	Hand();
-	~Hand();
+    // Constructor. 
+    //    Assigns initial amount of money
+    Player(int m);
+    int get_money();
+    void set_bet(int& b);
+    void reset();
+    
 
 private:
+    int money;
+    int bet;
+    Hand hand;
+    int game_num;
 
+    // You decide what extra fields (if any) you'll need...
 };
 
+#endif
 
 
-class Card
-{
-public:
-	Card();
-	~Card();
-
-private:
-
-};
 
 
 
--- siete-y-medio.cpp
+++ siete-y-medio.cpp
@@ -1,7 +1,25 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 #include "cards.h"
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+// Global constants (if any)
+
+
+// Non member functions declarations (if any)
+
+
+// Non member functions implementations (if any)
+
+
+// Stub for main
+int main() {
+    Player you(100), dealer(900);
+    std::cout << "You have $";
+    return 0;
 }
